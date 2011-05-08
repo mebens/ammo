baee7613ed1c9945cb49a973697f24392ef1110d
Michael Ebens <michael@nova-fusion.com>
2011-05-08 11:33:56 +1000

    Added drag.

--- extras/Physical.lua
+++ extras/Physical.lua
@@ -1,11 +1,22 @@
 local function computeVelocity(self, axis)
-  local vel = 0
+  local vel = self.velocity[axis]
   local max = self.maxVelocity[axis]
   
   if self.acceleration[axis] ~= 0 then
-    vel = math.clamp(self.velocity[axis] + self.acceleration[axis] * dt, -max, max) / 2
+    vel = math.clamp(self.velocity[axis] + self.acceleration[axis] * dt, -max, max)
+  elseif self.drag[axis] ~= 0 tehn
+    local drag = self.drag[axis] * dt
+    
+    if vel - drag > 0 then
+      vel = vel - drag
+    elseif vel + drag < 0 then
+      vel = vel + drag
+    else
+      vel = 0
+    end
   end
   
+  vel = (vel - self.velocity[axis]) / 2
   self.velocity[axis] = self.velocity[axis] + vel
   self[axis] = self[axis] + self.velocity[axis] * dt
   self.velocity[axis] = self.velocity[axis] + vel
@@ -20,6 +31,7 @@ end
 function Phyiscal:initalizePhysics()
   self.velocity = Vector(0, 0)
   self.acceleration = Vector(0, 0)
+  self.drag = Vector(0, 0)
   self.maxVelocity = Vector(10000, 10000)
   -- possibly include anglular stuff in the future?
 end
