42ccccf20dacfa6d423f4fa6fa04eb45bff4535e
Michael Ebens <michael@nova-fusion.com>
2011-05-24 18:48:13 +1000

    A bit more with Editable. I might stop making a reusable editor, rather make one specific to Launcher, and then make it reuseable.

--- editor/Editable.lua
+++ editor/Editable.lua
@@ -3,14 +3,30 @@ Editable = {}
 function Editable:included(world)
   world:addFilter(Filter:new(), 'draw')
   
+  for e in world:getIterator() do
+    if instanceOf(Entity, e) then e:include(MouseInteractive) end
+  end
+  
+  function world:add(...)
+    for _, v in pairs{...} do
+      if not v._world then
+        self._add[#self._add + 1] = v
+        if instanceOf(Entity, v) then v:include(MouseInteractive) end
+      end
+    end
+  end
+  
   world._gui = {}
   -- add GUI elements
   for _, v in pairs(world._gui) do v._editor = true end
 end
 
 function Editable:update(dt)
-  for _, v in pairs(self._gui) do
-    if v.active then v:update(dt) end
+  for v in self._updates:getIterator() do
+    if v.active then
+      if v._editor then v:update(dt) end
+      if v.updateMouse then v:updateMouse() end
+    end
   end
   
   self:_updateLists()
