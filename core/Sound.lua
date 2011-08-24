4408172f764f713e427ed986b8d8434a6dc22075
Michael Ebens <michael@nova-fusion.com>
2011-08-25 07:58:28 +1000

    Refactored the Sound class.

--- core/Sound.lua
+++ core/Sound.lua
@@ -13,16 +13,25 @@ end
 
 Sound:enableAccessors()
 
-function Sound:initialize(file, volume, pan)
+function Sound:initialize(file, type, volume, pan)
   self._file = file
+  self._type = type or "short"
   self._sources = {}
   self.defaultVolume = volume or 1
   self.defaultPan = pan or 0
+  
+  if self._type == "short" then
+    self._data = love.sound.newSoundData(file)
+  else
+    self._data = file
+  end
+  
   self:applyAccessors()
   table.insert(love.audio._sounds, self)
 end
 
-function Sound:_play(source, volume, pan)
+function Sound:play(volume, pan)
+  local source = love.audio.newSource(self._data, "stream")
   source:setVolume(volume or self.defaultVolume)
   source:setPosition(pan or self.defaultPan, 0, 0)
   source:play()
@@ -38,25 +47,4 @@ for _, v in pairs{'pause', 'resume', 'rewind', 'stop'} do
       for _, s in pairs(self._sources) do s[v]() end
     end
   end
-end
-
--- SFX --
-
-Sfx = class('Sfx', Sound)
-
-function Sfx:initialize(file, volume, pan)
-  self._data = love.sound.newSoundData(file)
-  Sound.initialize(self)
-end
-
-function Sfx:play(volume, pan)
-  return self:_play(love.audio.newSource(self._data, 'stream'), volume, pan)
-end
-
--- MUSIC --
-
-Music = class('Music', Sound)
-
-function Music:play(volume, pan)
-  return self:_play(love.audio.newSource(self._file, 'stream'), volume, pan)
 end
