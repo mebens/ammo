d6c8772bb7d825ba247574ea64e509b134e5918d
Michael Ebens <michael@nova-fusion.com>
2012-01-14 21:47:05 +1100

    Made module files return a table instead of defining a global. The global is defined in init.lua. Also modified the code of input, moving key and mouse into the input module.

--- core/camera.lua
+++ core/camera.lua
@@ -1,4 +1,4 @@
-camera = setmetatable({}, {
+local t = setmetatable({}, {
   __index = function(self, key)
     return (key == "x" or key == "y") and self._pos[key] or rawget(self, "_" .. key)
   end,
@@ -16,61 +16,63 @@ camera = setmetatable({}, {
   end
 })
 
-camera._pos = Vector(0, 0)
-camera.zoom = 1
-camera.rotation = 0
+t._pos = Vector(0, 0)
+t.zoom = 1
+t.rotation = 0
 
-function camera.set(scale)
+function t.set(scale)
   local x, y
   
   if scale ~= 1 then 
-    x = camera.x
-    y = camera.y
-    camera.x = camera.x * scale
-    camera.y = camera.y * scale
+    x = t.x
+    y = t.y
+    t.x = t.x * scale
+    t.y = t.y * scale
   end
   
   love.graphics.push()
-  love.graphics.scale(camera.zoom)
-  love.graphics.rotate(camera.rotation)
-  love.graphics.translate(-camera.x, -camera.y)
+  love.graphics.scale(t.zoom)
+  love.graphics.rotate(t.rotation)
+  love.graphics.translate(-t.x, -t.y)
   
   if scale ~= 1 then
-    camera.x = x
-    camera.y = y
+    t.x = x
+    t.y = y
   end
 end
 
-function camera.unset()
+function t.unset()
   love.graphics.pop()
 end
 
-function camera.update(dt)
+function t.update(dt)
   
 end
 
-function camera.move(dx, dy)
-  camera.x = camera.x + dx
-  camera.y = camera.y + dy
+function t.move(dx, dy)
+  t.x = t.x + dx
+  t.y = t.y + dy
 end
 
-function camera.rotate(dr)
-  camera.rotation = camera.rotation + dr
+function t.rotate(dr)
+  t.rotation = t.rotation + dr
 end
 
-function camera.getPosition()
-  return camera.x, camera.y
+function t.getPosition()
+  return t.x, t.y
 end
 
-function camera.setPosition(x, y)
-  camera.x = x
-  camera.y = y
+function t.setPosition(x, y)
+  t.x = x
+  t.y = y
 end
 
-function camera.setBounds(x1, y1, x2, y2)
-  camera.bounds = { x1 = x1, y1 = y1, x2 = x2, y2 = y2 }
+function t.setBounds(x1, y1, x2, y2)
+  t.bounds = { x1 = x1, y1 = y1, x2 = x2, y2 = y2 }
 end
 
-function camera.processCoordinate(axis, value)
-  return camera.bounds and math.clamp(value, camera.bounds[axis .. "1"], camera.bounds[axis .. "2"]) or value
+function t.processCoordinate(axis, value)
+  return t.bounds and math.clamp(value, t.bounds[axis .. "1"], t.bounds[axis .. "2"]) or value
 end
+
+return t
--- core/input.lua
+++ core/input.lua
@@ -8,88 +8,94 @@ local mt = {
 
 -- INPUT --
 
-input = {}
-input._maps = {}
+t = {}
+t._maps = {}
 
-function input.define(t, ...)
+function t.define(t, ...)
   if type(t) == 'string' then
-    input._maps[t] = { key = {...} }
+    t._maps[t] = { key = {...} }
   else
     if type(t.key) == 'string' then t.key = { t.key } end
     if type(t.mouse) == 'string' then t.mouse = { t.mouse } end
-    input._maps[t[1]] = t
+    t._maps[t[1]] = t
   end
 end
 
-function input.pressed(name)
-  return input._check(name, 'pressed')
+function t.pressed(name)
+  return t._check(name, 'pressed')
 end
 
-function input.down(name)
-  return input._check(name, 'down')
+function t.down(name)
+  return t._check(name, 'down')
 end
 
-function input.released(name)
-  return input._check(name, 'released')
+function t.released(name)
+  return t._check(name, 'released')
 end
 
-function input._check(name, type)
-  local map = input._maps[name]
+function t._check(name, type)
+  local map = t._maps[name]
   
   if map.key then
     for _, v in pairs(map.key) do
-      if key[type][v] then return true end
+      if t.key[type][v] then return true end
     end
   end
   
   if map.mouse then
     for _, v in pairs(map.mouse) do
-      if mouse[type][v] then return true end
+      if t.mouse[type][v] then return true end
     end
   end
   
   return false
 end
 
-function input._event(e, a, b, c)
+function t._event(e, a, b, c)  
   if e == 'kp' then
-    key.pressed[a] = true
-    key.down[a] = true
-    key.pressed.count = key.pressed.count + 1
-    key.down.count = key.down.count + 1
+    local k = t.key
+    k.pressed[a] = true
+    k.down[a] = true
+    k.pressed.count = k.pressed.count + 1
+    k.down.count = k.down.count + 1
   elseif e == 'kr' then
-    key.released[a] = true
-    key.down[a] = nil
-    key.released.count = key.released.count + 1
-    key.down.count = key.down.count - 1
+    local k = t.key
+    k.released[a] = true
+    k.down[a] = nil
+    k.released.count = k.released.count + 1
+    k.down.count = k.down.count - 1
   elseif e == 'mp' then
-    mouse.pressed[c] = true
-    mouse.down[c] = true
-    mouse.pressed.count = mouse.pressed.count + 1
-    mouse.down.count = mouse.down.count + 1
+    local m = t.mouse
+    m.pressed[c] = true
+    m.down[c] = true
+    m.pressed.count = m.pressed.count + 1
+    m.down.count = m.down.count + 1
   elseif e == 'mr' then
-    mouse.released[c] = true
-    mouse.down[c] = nil
-    mouse.released.count = mouse.released.count + 1
-    mouse.down.count = mouse.down.count - 1
+    local m = t.mouse
+    m.released[c] = true
+    m.down[c] = nil
+    m.released.count = m.released.count + 1
+    m.down.count = m.down.count - 1
   end
 end
 
-function input._update()
-  key.pressed = setmetatable({ count = 0 }, mt)
-  key.released = setmetatable({ count = 0 }, mt)
-  mouse.pressed = setmetatable({ count = 0 }, mt)
-  mouse.released = setmetatable({ count = 0 }, mt)
-  mouse.x = love.mouse.getX()
-  mouse.y = love.mouse.getY()
+function t._update()
+  t.key.pressed = setmetatable({ count = 0 }, mt)
+  t.key.released = setmetatable({ count = 0 }, mt)
+  t.mouse.pressed = setmetatable({ count = 0 }, mt)
+  t.mouse.released = setmetatable({ count = 0 }, mt)
+  t.mouse.x = love.mouse.getX()
+  t.mouse.y = love.mouse.getY()
 end
 
 -- KEY AND MOUSE --
 
-key = {}
-mouse = {}
+t.key = {}
+t.mouse = {}
 
 for _, v in pairs{'pressed', 'down', 'released'} do
-  key[v] = setmetatable({ count = 0 }, mt)
-  mouse[v] = setmetatable({ count = 0 }, mt)
+  t.key[v] = setmetatable({ count = 0 }, mt)
+  t.mouse[v] = setmetatable({ count = 0 }, mt)
 end
+
+return t
