0f00386454acdace6493f69fdf2656efab66fdbf
Michael Ebens <michael@nova-fusion.com>
2013-09-01 13:41:11 +1000

    Only include MiddleClass if it hasn't already been included.

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 ammo = {}
 ammo.path = ({...})[1]:gsub("%.init", "")
 
-require(ammo.path .. ".lib.middleclass.middleclass")
+-- only include MiddleClass if it hasn't already been included
+if Object then require(ammo.path .. ".lib.middleclass.middleclass") end
 require(ammo.path .. ".core.init")
