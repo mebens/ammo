aa285bf7d65cc655206018749e34d8f8a2e615e4
Michael Ebens <michael@nova-fusion.com>
2014-01-13 08:16:30 +1000

    Fixed MiddleClass inclusion (should do anyway).

--- init.lua
+++ init.lua
@@ -2,5 +2,5 @@ ammo = {}
 ammo.path = ({...})[1]:gsub("%.init", "")
 
 -- only include MiddleClass if it hasn't already been included
-if Object then require(ammo.path .. ".lib.middleclass.middleclass") end
+if not Object then require(ammo.path .. ".lib.middleclass.middleclass") end
 require(ammo.path .. ".core.init")
