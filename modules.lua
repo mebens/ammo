5d84b278030afbc7d4299a519c3f4797170dae57
Michael Ebens <michael@nova-fusion.com>
2018-07-23 16:50:58 +1000

    modules.lua assigns debug to ammo.db.

--- modules.lua
+++ modules.lua
@@ -2,4 +2,5 @@ require(ammo.path .. ".audio")
 require(ammo.path .. ".tweens")
 input = require(ammo.path .. ".input")
 assets = require(ammo.path .. ".assets")
+ammo.db = require(ammo.path .. ".debug")
 if love.physics then require(ammo.path .. ".physics") end
