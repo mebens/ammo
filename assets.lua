8df63e3dba952f6da99a3f846cf8421f549723c2
Michael Ebens <michael@nova-fusion.com>
2018-07-28 11:50:37 +1000

    Assets module now auto handles the assigning of loaded assets by default. First argument of load functions is now assets module.

--- assets.lua
+++ assets.lua
@@ -52,27 +52,38 @@ function assets.get(key)
 end
 
 function assets.newType(name, plural, path, types, loadFunc)
+  plural = plural or name .. "s"
+  path = path or plural
+
+  local function loadAsset(...)
+    local obj, objName, assign = loadFunc(assets, ...)
+
+    if assign ~= false then
+      assets[plural][objName] = obj
+    end
+
+    return obj, objName
+  end
+
   local t = setmetatable({}, {
     __call = function(self, ...)
       local args = {...}
 
       if #args == 1 and type(args[1]) == "table" then
         for key, val in pairs(args[1]) do
-          loadFunc(val, type(key) ~= "number" and key or nil)
+          loadAsset(val, type(key) ~= "number" and key or nil)
         end
       else
         for _, f in ipairs(args) do
-          loadFunc(f)
+          loadAsset(f)
         end
       end
     end
   })
 
-  plural = plural or name .. "s"
-  path = path or plural
   assets[name] = t
   assets[plural] = t
-  assets["new" .. name:gsub("^.", string.upper)] = loadFunc
+  assets["new" .. name:gsub("^.", string.upper)] = loadAsset
   assets[name .. "Path"] = path
 
   if types then
@@ -82,9 +93,9 @@ function assets.newType(name, plural, path, types, loadFunc)
   assets["all" .. plural:gsub("^.", string.upper)] = function()
     for _, f in ipairs(love.filesystem.getDirectoryItems(assets.getPath(nil, name))) do
       if not assets.types[name] then
-        loadFunc(f)
+        loadAsset(f)
       elseif matchType(f, name) then
-        loadFunc(f)
+        loadAsset(f)
       end
     end
   end
@@ -114,107 +125,112 @@ function assets.getPath(file, type)
   return (assets.path and (assets.path .. "/") or "") .. assets[type .. "Path"] .. "/" .. (file or "")
 end
 
-assets.newType("image", nil, nil, { "png", "jpg", "jpeg", "tga", "bmp" }, function (file, name)
-  local img = love.graphics.newImage(assets.getPath(file, "image"))
-  rawset(assets.images, name or assets.getName(file), img)
-  return img
-end)
-
-assets.newType("sfx", "sfx", nil, { "ogg", "oga", "wav", "mp3" }, function (file, name, pool, volume, stream)
-  if type(name) == "boolean" then
-    stream = volume
-    volume = pool
-    pool = name
-    name = nil
-  elseif type(name) == "number" then
-    volume = name
-    stream = pool
-    pool = false
-    name = nil
+assets.newType("image", nil, nil, { "png", "jpg", "jpeg", "tga", "bmp" },
+  function (self, file, name)
+    return love.graphics.newImage(self.getPath(file, "image")), name or self.getName(file)
   end
-  
-  if type(file) == "table" then
-    if not name then
-      name = assets.getName(file[1], true)
+)
+
+assets.newType("sfx", "sfx", nil, { "ogg", "oga", "wav", "mp3" },
+  function (self, file, name, pool, volume, stream)
+    if type(name) == "boolean" then
+      stream = volume
+      volume = pool
+      pool = name
+      name = nil
+    elseif type(name) == "number" then
+      volume = name
+      stream = pool
+      pool = false
+      name = nil
+    end
+    
+    if type(file) == "table" then
+      if not name then
+        name = self.getName(file[1], true)
+      end
+
+      for i, v in ipairs(file) do
+        file[i] = self.getPath(file[i], "sfx")
+      end
+    else
+      file = self.getPath(file, "sfx")
     end
 
-    for i, v in ipairs(file) do
-      file[i] = assets.getPath(file[i], "sfx")
+    local sound
+
+    if pool then
+      sound = SoundPool:new(file, volume or 1, stream)
+    elseif Sound then
+      sound = Sound:new(file, volume or 1, stream)
+    else
+      sound = love.audio.newSource(file, stream and "stream" or "static")
+      if volume then sound:setVolume(volume) end
     end
-  else
-    file = assets.getPath(file, "sfx")
+
+    return sound, name or self.getName(file)
   end
+)
 
-  local sound
+assets.newType("music", "music", nil, nil,
+  function (self, file, name, volume)
+    if type(name) == "number" then
+      volume = name
+      name = nil
+    end
 
-  if pool then
-    sound = SoundPool:new(file, volume or 1, stream)
-  elseif Sound then
-    sound = Sound:new(file, volume or 1, stream)
-  else
-    sound = love.audio.newSource(file, stream and "stream" or "static")
-    if volume then sound:setVolume(volume) end
+    local sound
+
+    if Sound then
+      sound = Sound:new(self.getPath(file, "music"), volume or 1, true)
+    else
+      sound = love.audio.newSource(file, "stream")
+      if volume then sound:setVolume(volume) end
+    end
+    
+    return sound, name or self.getName(file)
   end
+)
 
-  rawset(assets.sfx, name or assets.getName(file), sound)
-  return sound
-end)
+assets.newType("shader", nil, nil, { "frag", "vert", "glsl", "shader" },
+  function (self, file, fileOrName, name)
+    local source = love.filesystem.read(self.getPath(file, "shader"))
+    local shader
 
-assets.newType("music", "music", nil, nil, function (file, name, volume)
-  if type(name) == "number" then
-    volume = name
-    name = nil
+    if name then
+      local source2 = love.filesystem.read(self.getPath(fileOrName, "shader"))
+      shader = love.graphics.newShader(source, source2)
+    else
+      shader = love.graphics.newShader(source)
+      name = fileOrName
+    end
+
+    return shader, name or self.getName(file)
   end
+)
 
-  local sound
+assets.newType("font", nil, nil, { "ttf", "otf" },
+  function (self, file, size, name)
+    -- support for assets() syntax
+    if type(file) == "table" then
+      size = file[2]
+      name = file[3]
+      file = file[1]
+    end
 
-  if Sound then
-    sound = Sound:new(assets.getPath(file, "music"), volume or 1, true)
-  else
-    sound = love.audio.newSource(file, "stream")
-    if volume then sound:setVolume(volume) end
+    name = name or self.getName(file)
+    size = size or 12
+
+    if type(size) == "table" then
+      for i = 1, #size do self.newFont(file, size[i], name) end
+      return self.fonts[name], name, false
+    end
+    
+    local font = love.graphics.newFont(self.getPath(file, "font"), size)
+    if not self.fonts[name] then self.fonts[name] = {} end
+    self.fonts[name][size] = font
+    return font, name, false
   end
-  
-  rawset(assets.music, name or assets.getName(file), sound)
-  return sound
-end)
-
-assets.newType("shader", nil, nil, { "frag", "vert", "glsl", "shader" }, function (file, fileOrName, name)
-  local source = love.filesystem.read(assets.getPath(file, "shader"))
-  local shader
-
-  if name then
-    local source2 = love.filesystem.read(assets.getPath(fileOrName, "shader"))
-    shader = love.graphics.newShader(source, source2)
-  else
-    shader = love.graphics.newShader(source)
-    name = fileOrName
-  end
-
-  rawset(assets.shaders, name or assets.getName(file), shader)
-  return shader
-end)
-
-assets.newType("font", nil, nil, { "ttf", "otf" }, function (file, size, name)
-  -- support for assets.fonts() syntax
-  if type(file) == "table" then
-    size = file[2]
-    name = file[3]
-    file = file[1]
-  end
-
-  name = name or assets.getName(file)
-  size = size or 12
-
-  if type(size) == "table" then
-    for i = 1, #size do assets.newFont(file, size[i], name) end
-    return assets.fonts[name]
-  end
-  
-  local font = love.graphics.newFont(assets.getPath(file, "font"), size)
-  if not assets.fonts[name] then assets.fonts[name] = {} end
-  assets.fonts[name][size] = font
-  return font
-end)
+)
 
 return assets
